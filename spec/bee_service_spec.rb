038764bc957ac3f6cca6ac110ed5e6bc03466225
Kingdon Barrett <kingdon.b@nd.edu>
2019-04-13 17:50:50 -0400

    (red) more pending specs

---
+++ spec/bee_service_spec.rb
@@ -0,0 +1,30 @@
+require 'bee_service'
+
+RSpec.describe BeeService do
+  describe '#seen_promise?' do
+    it 'returns true if the slug+promise is in json' do
+      pending
+    end
+    it 'returns false if the slug+promise is not in json' do
+      pending
+    end
+  end
+  describe '#seen_completed?' do
+    it 'returns true if the slug+success is in json' do
+      pending
+    end
+    it 'returns false if the slug+success is not in json' do
+      pending
+    end
+  end
+  describe '#log_promise' do
+    it 'adds a promise with the parameter timestamp and url slug' do
+      pending
+    end
+  end
+  describe '#log_completed' do
+    it 'adds a success with the parameter timestamp and url slug' do
+      pending
+    end
+  end
+end
