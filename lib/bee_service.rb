163d2496d8086c5279f11266f57f2cdbef12cea3
Kingdon Barrett <kingdon.b@nd.edu>
2019-04-14 11:04:25 -0400

    (green) BeeService seen_promise? and _completed?

--- lib/bee_service.rb
+++ lib/bee_service.rb
@@ -16,13 +16,11 @@ class BeeService
   end
 
   def seen_promise?(slug)
-    json_data
-    # binding.pry
-    true
+    promise_slugs.include? slug
   end
 
   def seen_completed?(slug)
-    # binding.pry
+    completed_slugs.include? slug
   end
 
   class NotImplementedYet < StandardError; end
@@ -35,31 +33,64 @@ class BeeService
   end
 
   private
-  def validate_json
-    if json.class.to_s == "Hash"
-      status = json["status"]
-      error = json["error"]
+    def validate_json
+      if json.class.to_s == "Hash"
+        status = json["status"]
+        error = json["error"]
 
-      if status == "404"
-        raise JsonError, "#{status} #{error}"
+        if status == "404"
+          raise JsonError, "#{status} #{error}"
+        end
+        raise JsonError, json["error"] if json.has_key?("error")
+      elsif json.class.to_s == "Array"
+        raise JsonError, "no data points" if json.count < 1
+      else
+        raise JsonError, "no data was parsed"
       end
-      raise JsonError, json["error"] if json.has_key?("error")
-    elsif json.class.to_s == "Array"
-      raise JsonError, "no data points" if json.count < 1
-    else
-      raise JsonError, "no data was parsed"
-    end
 
-    raise CurlError, "JSON data isn't fresh, try ./README" unless fresh_json_data?
-  end
-  def json
-    @json ||= JSON.parse(json_data)
-  end
+      raise CurlError, "JSON data isn't fresh, try ./README" unless fresh_json_data?
+    end
+    def json
+      @json ||= JSON.parse(json_data)
+    end
 
     # JSON is considered fresh if it has an mtime in 10 seconds recent history
     def fresh_json_data?
       mtime = File.mtime(json_filename)
       Time.now - mtime < 10
     end
+
+    def comment_matcher
+      /(promise|success): (.*)/
+    end
+
+    def all_matched_comments
+      @all_matched_comments ||=
+        json.map{|h| h["comment"]}.
+        select{|c| c[comment_matcher]}
+    end
+
+    def all_matched_tuples
+      @all_matched_tuples ||=
+        all_matched_comments.map do |c|
+        m = comment_matcher.match c
+        [m[1], m[2]]
+      end
+    end
+
+    def promise_slugs
+      @promise_slugs ||=
+        all_matched_tuples.
+        select{|m| m[0] == "promise"}.
+        map{|p| p[1]}
+    end
+
+    def completed_slugs
+      @completed_slugs ||=
+        all_matched_tuples.
+        select{|m| m[0] == "success"}.
+        map{|s| s[1]}
+    end
+
   #
 end
