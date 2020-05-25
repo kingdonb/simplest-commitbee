7440e32014ca11809e430df221edecbc5e2c1061
Kingdon Barrett <kingdon.b@nd.edu>
2020-05-25 12:55:01 -0400

    set content-type as json on HTTP service too
    Remember this, or your browser is likely to interpolate some radical
    nonsense such as: <html><head></head><body>{"ok":true}</body></html>

--- web.rb
+++ web.rb
@@ -8,7 +8,7 @@ while session = server.accept
   # puts request
 
   session.print "HTTP/1.1 200\r\n" # 1
-  session.print "Content-Type: text/html\r\n" # 2
+  session.print "Content-Type: application/json\r\n" # 2
   session.print "\r\n" # 3
   session.print '{"ok":true}' #4
 
